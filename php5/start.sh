dbf9bc4c2bc5b715a868aba776a0ea675268a4a0
xingyue <qixingyue@126.com>
2015-09-11 14:43:21 +0800

    添加name

--- php5/start.sh
+++ php5/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 d=$(pwd)
-dparams="-i -t -p 9000  -v $d/code:/code"
+dparams="--name php5 -i -t -p 9000  -v $d/code:/code"
 dparams="$dparams --privileged=true "
 screen -s dockernginx docker run $dparams qdocker/php5:7 
 #docker run $dparams qdocker/php5:7 
