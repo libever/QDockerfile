0ee27850c4d3b336f597dc96fdfba1031bea80f4
xingyue <qixingyue@126.com>
2015-09-11 15:31:15 +0800

    添加启动参数，公用code目录

--- php5/start.sh
+++ php5/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-d=$(pwd)
-dparams="--name php5 -i -t -p 9000  -v $d/code:/code"
+d=$(readlink -f ../code)
+dparams="--name php5 -i -t -p 9000  -v $d:/code"
 dparams="$dparams --privileged=true "
 screen -s dockernginx docker run $dparams qdocker/php5:7 
-#docker run $dparams qdocker/php5:7 
+#docker run $dparams qdocker/php5:7
