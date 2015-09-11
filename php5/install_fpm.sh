f460a220473b250f0235721b77ae70f97fcb3c27
xingyue <qixingyue@126.com>
2015-09-11 11:13:16 +0800

    修改php5镜像，以php-fpm方式运行

--- php5/install_fpm.sh
+++ php5/install_fpm.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-cd /sw/nginx-1.9.3
-./configure --prefix=/nginx && make && make install
+cd /sw/php-5.4.20
+./configure --prefix=/php5 --enable-mysqlnd --with-mysql --with-mysqli --with-pdo-mysql --enable-fpm --enable-pcntl 
+make 
+make install
+cp php.ini-development /php5/lib/php.ini
+mv /php5/etc/php-fpm.conf.default /php5/etc/php-fpm.conf
 
+echo "#ADD PHP PATH" >> /etc/bashrc
+echo "PATH=\$PATH:/php5/bin" >> /etc/bashrc
