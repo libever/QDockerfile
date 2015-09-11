998d0de9f7676f10c15e8cfe7bf853180db22d65
xingyue <qixingyue@126.com>
2015-09-11 15:33:36 +0800

    修改php screen启动参数

--- php5/start.sh
+++ php5/start.sh
@@ -3,5 +3,5 @@
 d=$(readlink -f ../code)
 dparams="--name php5 -i -t -p 9000  -v $d:/code"
 dparams="$dparams --privileged=true "
-screen -s dockernginx docker run $dparams qdocker/php5:7 
+screen -S dockerphp5 docker run $dparams qdocker/php5:7 
 #docker run $dparams qdocker/php5:7
