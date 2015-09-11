e60f3272d6258cc449f115126d1dbeee0bbc2088
xingyue <qixingyue@126.com>
2015-09-11 15:36:52 +0800

    修改nginx启动脚本,映射本地code目录

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-screen -S dockernginx docker run -i -t -p 8000:80  qdocker/nginx:7 
+d=$(readlink -f ../code)
+dparams="--name nginx -i -t -p 8000:80 -v $d:/code"
+dparams="$dparams --privileged=true "
+screen -S dockernginx docker run $dparams qdocker/nginx:7 
