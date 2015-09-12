efceaa31eee58574dd3cb6144f1663b25551aeb8
xingyue <qixingyue@126.com>
2015-09-12 20:28:27 +0800

    when have no php-5.4.20 file , try to get from museum.php.net

--- php5/build.sh
+++ php5/build.sh
@@ -6,4 +6,12 @@ then
 docker build -t qdocker/php5:7 .
 else
 echo "need file : php-5.4.20.tar.gz"
+echo "download file : "
+wget -t 3 --timeout 180 "http://museum.php.net/php5/php-5.4.20.tar.gz" 
+if [ $? -eq 0 ]
+then
+	sh build.sh
+else
+	rm php-5.4.20.tar.gz
+fi
 fi
