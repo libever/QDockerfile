e1b50da36d9b9e519046bd47db718863da685fd0
xingyue <qixingyue@126.com>
2015-09-17 10:31:36 +0800

    修改脚本写法，可以添加多个文件下载包

--- webpy/build.sh
+++ webpy/build.sh
@@ -1,22 +1,30 @@
 #!/bin/sh
 
-file_name="web.py-0.33.tar.gz"
-file_url="http://webpy.org/static/web.py-0.33.tar.gz"
+function check_file() {
+	file_name=$1
+	file_url=$2
+	if [ -f ./packages/$file_name ] 
+	then
+		echo "chekfile $file_name : OK "
+	else
+		echo "need file : $file_name"
+		echo "download file : "
+		wget --no-check-certificate -t 6 --timeout 180 $file_url -O packages/$file_name
+		if [ $? -eq 0 ]
+		then
+			echo "download $file_name finished "
+		else
+			rm $file_name
+			echo "################"
+			echo "$file_name get error ... try to change other version of webpy ."
+			exit
+		fi
+	fi
+}
 
-if [ -f $file_name ] 
-then
+mkdir -p packages
+check_file "web.py-0.33.tar.gz" "http://webpy.org/static/web.py-0.33.tar.gz"
+check_file "requests-2.7.0.tar.gz" "https://pypi.python.org/packages/source/r/requests/requests-2.7.0.tar.gz#md5=29b173fd5fa572ec0764d1fd7b527260"
+
+
 docker build -t qdocker/webpy:7 .
-else
-echo "need file : $file_name"
-echo "download file : "
-wget -t 3 --timeout 180 $file_url
-if [ $? -eq 0 ]
-then
-	sh build.sh
-else
-	rm $file_name
-	echo "################"
-	echo "$file_name get error ... try to change other version of webpy ."
-fi
-fi
-
--- webpy/install_webpy.sh
+++ webpy/install_webpy.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
-cd /sw/web.py-0.33
-python setup.py install
+cd /sw/packages
+
+for pakckageName in $(ls)
+do
+	tar zxf $pakckageName
+done
+
+for pdir in $(ls -F | grep "/$")
+do
+	d=$(pwd)
+	echo $pdir
+	cd $pdir
+	python setup.py install
+	cd $d
+done
