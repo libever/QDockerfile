b0819b3193a618455028dfc286d4c76f5ba668ee
xingyue <qixingyue@126.com>
2015-09-18 16:16:33 +0800

    运行web.py测试脚本

--- webpy/build.sh
+++ webpy/build.sh
@@ -15,7 +15,7 @@ function check_file() {
 			echo "download $file_name finished "
 		else
 			rm $file_name
-			echo "################"
+			echo "### ERROR ####"
 			echo "$file_name get error ... try to change other version of webpy ."
 			exit
 		fi
@@ -25,6 +25,5 @@ function check_file() {
 mkdir -p packages
 check_file "web.py-0.33.tar.gz" "http://webpy.org/static/web.py-0.33.tar.gz"
 check_file "requests-2.7.0.tar.gz" "https://pypi.python.org/packages/source/r/requests/requests-2.7.0.tar.gz#md5=29b173fd5fa572ec0764d1fd7b527260"
-
 
 docker build -t qdocker/webpy:7 .
--- webpy/start.sh
+++ webpy/start.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 d=$(greadlink -f ../code)
-dparams="--name nginx -i -t -p 8000:80 -v $d:/code"
+dparams="--name nginx -i -t -p 8000:80 -v $d:/code -w /code"
 dparams="$dparams --privileged=true "
 screen -S dockerwebpy docker run $dparams qdocker/webpy:7 
-
