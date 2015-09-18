5e38247935de2595745ffc81c9048241deb378d7
xingyue <qixingyue@126.com>
2015-09-18 16:16:01 +0800

    运行测试脚本

--- webpy/run_demo.sh
+++ webpy/run_demo.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+
+cat >> /sw/demo.py <<EOF
+
 #!/usr/bin/env python
 #coding=utf-8
 
@@ -14,3 +18,8 @@ if __name__ == "__main__":
     app.run()
 else :
     application = app.wsgifunc()
+
+EOF
+
+cd /sw/
+python demo.py > r.log 2>&1 & 
