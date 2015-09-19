48ced2e3ba859eae354a77d94e89221745a8a512
xingyue <qixingyue@126.com>
2015-09-19 12:23:54 +0800

    修复nginx启动脚本

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-d=$(readlink -f ../code)
+SYSTEM=`uname -s` 
+if [  $SYSTEM = "Darwin" ] 
+then
+	d=$(greadlink -f ../code)
+else 
+	d=$(readlink -f ../code)
+fi
+
+
 dparams="--name nginx -i -t -p 8000:80 -v $d:/code"
 dparams="$dparams --privileged=true "
 screen -S dockernginx docker run $dparams qdocker/nginx:7 
