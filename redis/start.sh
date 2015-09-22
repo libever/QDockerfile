364f74633a30357bdb2537d92ef9ab858fc26709
qixingyue <qixingyue@126.com>
2015-09-22 18:30:08 +0800

    加入几种状态监测

--- redis/start.sh
+++ redis/start.sh
@@ -10,10 +10,16 @@ fi
 mkdir -p $vdir
 dparams="--name redis -i -t -p 8001:6379 -v $vdir:/blackhole"
 
-r=$(docker exec redis /bin/sh -c "echo OK")
+r=$(docker exec redis /bin/sh -c "echo OK" 2>&1 )
 if [ "$r" == "OK" ]
 then
+    echo "already running  "
 	screen -x dockerredis
-else
+elif [[ "$r" =~ ".*no\ssuch\sid.*" ]]
+then
+    echo "not exist , not running , run  "
 	screen -S dockerredis docker run $dparams qdocker/redis:7 
+else
+    echo "exist , not running , run and attach "
+    screen -S dockerredis /bin/bash -c "docker start redis && docker attach redis "
 fi
