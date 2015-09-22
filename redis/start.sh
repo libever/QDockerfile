c2a2ce82f8d9cb707239cf0a42a70e02518d9c76
xingyue <qixingyue@126.com>
2015-09-22 10:34:25 +0800

    修改redis启动脚本，添加本地目录，用来编写redis api 操作脚本

--- redis/start.sh
+++ redis/start.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-dparams="--name redis -i -t -p 8001:6379 "
-
+SYSTEM=$(uname -s)
+if [ $SYSTEM == "Darwin" ] 
+then
+	vdir=$(greadlink -f "../code/dockerredis")	
+else
+	vdir=$(readlink -f "../code/dockerredis")	
+fi
+mkdir -p $vdir
+dparams="--name redis -i -t -p 8001:6379 -v $vdir:/blackhole"
 screen -S dockerredis docker run $dparams qdocker/redis:7 
