e3f7175a91e3ed8544752e428765676135a71db0
xingyue <qixingyue@126.com>
2015-09-23 11:25:00 +0800

    修改php5的启动脚本

--- php5/start.sh
+++ php5/start.sh
@@ -1,7 +1,26 @@
 #!/bin/sh
 
-d=$(readlink -f ../code)
-dparams="--name php5 -i -t -p 9000  -v $d:/code"
+SYSTEM=$(uname -s)
+if [ $SYSTEM == "Darwin" ] 
+then
+	vdir=$(greadlink -f "../code/dockerphp5")	
+else
+	vdir=$(readlink -f "../code/dockerphp5")	
+fi
+mkdir -p $vdir
+dparams="--name php5 -i -t -p 9000  -v $vdir:/blckhole -w /blackhole "
 dparams="$dparams --privileged=true "
-screen -S dockerphp5 docker run $dparams qdocker/php5:7 
-#docker run $dparams qdocker/php5:7
+
+r=$(docker exec php5 /bin/sh -c "echo OK" 2>&1 )
+if [ "$r" == "OK" ]
+then
+	echo "already running  "
+	screen -x dockerphp5 
+elif [[ "$r" =~ "no such id" ]]
+then
+  echo "not exist , not running , run  "
+	screen -S dockerphp5 docker run $dparams qdocker/php5:7 
+else
+    echo "exist , not running , run and attach "
+    screen -S dockerphp5 /bin/bash -c "docker start php5 && docker attach php5"
+fi
