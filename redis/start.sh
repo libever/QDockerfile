d84db5c1c02f22522aaa8800c2011bc86466fef0
xingyue <qixingyue@126.com>
2015-09-23 10:21:03 +0800

    匹配结果

--- redis/start.sh
+++ redis/start.sh
@@ -7,18 +7,20 @@ then
 else
 	vdir=$(readlink -f "../code/dockerredis")	
 fi
+
 mkdir -p $vdir
-dparams="--name redis -i -t -p 8001:6379 -v $vdir:/blackhole"
+dparams="--name redis -i -t -p 8001:6379 -v $vdir:/blackhole "
 
 r=$(docker exec redis /bin/sh -c "echo OK" 2>&1 )
+#echo $r
 if [ "$r" == "OK" ]
 then
-    echo "already running  "
+	echo "already running  "
 	screen -x dockerredis
-elif [[ "$r" =~ ".*no\ssuch\sid.*" ]]
+elif [[ "$r" =~ "no such id" ]]
 then
-    echo "not exist , not running , run  "
-	screen -S dockerredis docker run $dparams qdocker/redis:7 
+   echo "not exist , not running , run  "
+	 screen -S dockerredis docker run $dparams qdocker/redis:7 
 else
     echo "exist , not running , run and attach "
     screen -S dockerredis /bin/bash -c "docker start redis && docker attach redis "
