33f2ea6af5e7a1ddcfbfafe2d9423c16aeddd0a1
xingyue <qixingyue@126.com>
2015-09-25 17:54:49 +0800

    修改启动golang的脚本

--- golang/start.sh
+++ golang/start.sh
@@ -1,13 +1,41 @@
 #!/bin/sh
 
-SYSTEM=`uname -s` 
-if [  $SYSTEM = "Darwin" ] 
-then
-	d=$(greadlink -f ../code)
-else 
-	d=$(readlink -f ../code)
-fi
+#custom_edit_code 
 
-dparams="--name golang -i -t -v $d:/code"
+docker_name=golang
 dparams="$dparams --privileged=true "
-screen -S dockergolang docker run $dparams qdocker/golang:7 
+image_name=qdocker/golang:7
+docker_name=golang
+screen_name=docker${docker_name}
+blackhole_real_dir="../code/$screen_name"
+o_ports=" -p 9001:9001"
+
+#end custom_edit_code 
+
+
+
+SYSTEM=$(uname -s)
+if [ $SYSTEM == "Darwin" ] 
+then
+	Greadlink=greadlink
+else
+	Greadlink=readlink
+fi
+vdir=$($Greadlink -f $blackhole_real_dir)	
+echo $vdir
+mkdir -p $vdir
+dparams="--name $docker_name -i -t $o_ports  -v $vdir:/blackhole -w /blackhole"
+
+r=$(docker exec $docker_name /bin/sh -c "echo OK" 2>&1 )
+if [ "$r" == "OK" ]
+then
+	echo "already running  "
+	screen -x $screen_name
+elif [[ "$r" =~ "no such id" ]]
+then
+   echo "not exist , not running , run  "
+	 screen -S $screen_name docker run $dparams $image_name
+else
+    echo "exist , not running , run and attach "
+    screen -S $screen_name /bin/bash -c "docker start $docker_name && docker attach $docker_name"
+fi
