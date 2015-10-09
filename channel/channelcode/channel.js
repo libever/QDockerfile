2522c055bd74826c705078ab222c2ebdd633f446
qixingyue <qixingyue@126.com>
2015-10-09 16:49:16 +0800

    添加channel

---
+++ channel/channelcode/channel.js
@@ -0,0 +1,14 @@
+(function(){
+var channel = {
+
+	init:function(expressAddr,socketIoAddr){
+
+	}
+
+};
+
+exports.init = function(expressAddr,socketIoAddr){
+	channel.init(expressAddr,socketIoAddr);
+	return channel;
+}
+})();
