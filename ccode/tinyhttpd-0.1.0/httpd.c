79dd0f6f7694e149b90f094138386e13edb38b75
xingyue <qixingyue@126.com>
2015-10-14 14:30:25 +0800

    加入ctrl+c的信号处理

--- ccode/tinyhttpd-0.1.0/httpd.c
+++ ccode/tinyhttpd-0.1.0/httpd.c
@@ -12,22 +12,7 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 
-#define ISspace(x) isspace((int)(x))
-
-#define SERVER_STRING "Server: jdbhttpd/0.1.0\r\n"
-
-void accept_request(int*);
-void bad_request(int);
-void cat(int, FILE *);
-void cannot_execute(int);
-void error_die(const char *);
-void execute_cgi(int, const char *, const char *, const char *);
-int get_line(int, char *, int);
-void headers(int, const char *);
-void not_found(int);
-void serve_file(int, const char *);
-int startup(u_short *);
-void unimplemented(int);
+#include "httpd.h"
 
 void accept_request(int *transfterclient) {
 	int client = *transfterclient;
@@ -113,17 +98,15 @@ void accept_request(int *transfterclient) {
 }
 
 void bad_request(int client) {
-	char buf[1024];
-	sprintf(buf, "HTTP/1.0 400 BAD REQUEST\r\n");
-	send(client, buf, sizeof(buf), 0);
-	sprintf(buf, "Content-type: text/html\r\n");
-	send(client, buf, sizeof(buf), 0);
-	sprintf(buf, "\r\n");
-	send(client, buf, sizeof(buf), 0);
-	sprintf(buf, "<P>Your browser sent a bad request, ");
-	send(client, buf, sizeof(buf), 0);
-	sprintf(buf, "such as a POST without a Content-Length.\r\n");
-	send(client, buf, sizeof(buf), 0);
+	send(client, BADSTRING, strlen(BADSTRING), 0);
+}
+
+void cannot_execute(int client) {
+	send(client, INTERNAL_SERVER_ERROR_STRING, strlen(INTERNAL_SERVER_ERROR_STRING), 0);
+}
+
+void unimplemented(int client) {
+	send(client, METHOD_NOT_FOUND_STRING , strlen(METHOD_NOT_FOUND_STRING), 0);
 }
 
 void cat(int client, FILE *resource) {
@@ -133,18 +116,6 @@ void cat(int client, FILE *resource) {
 		send(client, buf, strlen(buf), 0);
 		fgets(buf, sizeof(buf), resource);
 	}
-}
-
-void cannot_execute(int client) {
-	char buf[1024];
-	sprintf(buf, "HTTP/1.0 500 Internal Server Error\r\n");
-	send(client, buf, strlen(buf), 0);
-	sprintf(buf, "Content-type: text/html\r\n");
-	send(client, buf, strlen(buf), 0);
-	sprintf(buf, "\r\n");
-	send(client, buf, strlen(buf), 0);
-	sprintf(buf, "<P>Error prohibited CGI execution.\r\n");
-	send(client, buf, strlen(buf), 0);
 }
 
 void error_die(const char *sc) {
@@ -248,11 +219,8 @@ int get_line(int sock, char *buf, int size) {
 
 	while ((i < size - 1) && (c != '\n')) {
 		n = recv(sock, &c, 1, 0);
-		/* DEBUG printf("%02X\n", c); */
 		if (n > 0) {
 			if (c == '\r') {
-				n = recv(sock, &c, 1, MSG_PEEK);
-				/* DEBUG printf("%02X\n", c); */
 				if ((n > 0) && (c == '\n')) {
 					recv(sock, &c, 1, 0);
 				} else {
@@ -356,47 +324,40 @@ int startup(u_short *port) {
 	return(httpd);
 }
 
-void unimplemented(int client) {
-	char buf[1024];
-	sprintf(buf, "HTTP/1.0 501 Method Not Implemented\r\n");
-	send(client, buf, strlen(buf), 0);
-	sprintf(buf, SERVER_STRING);
-	send(client, buf, strlen(buf), 0);
-	sprintf(buf, "Content-Type: text/html\r\n");
-	send(client, buf, strlen(buf), 0);
-	sprintf(buf, "\r\n");
-	send(client, buf, strlen(buf), 0);
-	sprintf(buf, "<HTML><HEAD><TITLE>Method Not Implemented\r\n");
-	send(client, buf, strlen(buf), 0);
-	sprintf(buf, "</TITLE></HEAD>\r\n");
-	send(client, buf, strlen(buf), 0);
-	sprintf(buf, "<BODY><P>HTTP request method not supported.\r\n");
-	send(client, buf, strlen(buf), 0);
-	sprintf(buf, "</BODY></HTML>\r\n");
-	send(client, buf, strlen(buf), 0);
+
+void StopServer(){
+	close(server_socket);
+	_exit(0);
 }
 
 int main(void) {
 
-	int server_sock = -1;
+	int timeoutsetres;
+	/*int server_sock = -1;*/
 	u_short port = 1025;
 	int client_sock = -1;
 	struct sockaddr_in client_name;
 	int client_name_len = sizeof(client_name);
+	struct timeval timeout={3,0};
 	pthread_t newthread;
 
-	server_sock = startup(&port);
+	server_socket = startup(&port);
 	printf("httpd running on port %d\n", port);
+	signal(SIGINT, StopServer); 
 
 	while (1) {
-		client_sock = accept(server_sock, (struct sockaddr *)&client_name, (socklen_t *)&client_name_len);
+		client_sock = accept(server_socket, (struct sockaddr *)&client_name, (socklen_t *)&client_name_len);
+		timeoutsetres = setsockopt(client_sock,SOL_SOCKET,SO_SNDTIMEO,(const char*)&timeout,sizeof(timeout));
+		timeoutsetres = setsockopt(client_sock,SOL_SOCKET,SO_RCVTIMEO,(const char*)&timeout,sizeof(timeout));
+
 		if (client_sock == -1) {
 			error_die("accept");
 		}
+
 		if (pthread_create(&newthread , NULL, (void *(*)(void *))accept_request, & client_sock) != 0) {
 			perror("pthread_create");
 		}
 	}
-	close(server_sock);
+	close(server_socket);
 	return(0);
 }
---
+++ ccode/tinyhttpd-0.1.0/httpd.h
@@ -0,0 +1,47 @@
+#ifndef HTTPD_COMMON_STRING_H
+#define HTTPD_COMMON_STRING_H
+
+
+#define SERVER_STRING "Server: jdbhttpd/0.1.0\r\n"
+#define BADSTRING "HTTP/1.0 400 BAD REQUEST\r\n\
+Server: jdbhttpd/0.1.0\r\n\
+Content-type: text/html\r\n\
+\r\n\
+<P>Your browser sent a bad request,uch as a POST without a Content-Length.\r\n"
+
+#define INTERNAL_SERVER_ERROR_STRING "HTTP/1.0 500 Internal Server Error\r\n\
+Server: jdbhttpd/0.1.0\r\n\
+Content-type: text/html\r\n\
+\r\n\
+<P>Error prohibited CGI execution.\r\n"
+
+
+#define METHOD_NOT_FOUND_STRING "HTTP/1.0 501 Method Not Implemented\r\n\
+Server: jdbhttpd/0.1.0\r\n\
+Content-Type: text/html\r\n\
+\r\n\
+<HTML><HEAD><TITLE>Method Not Implemented\r\n\
+</TITLE></HEAD>\r\n\
+<BODY><P>HTTP request method not supported.\r\n\
+</BODY></HTML>\r\n"
+
+static int server_socket;
+
+
+#define ISspace(x) isspace((int)(x))
+void accept_request(int*);
+void bad_request(int);
+void cat(int, FILE *);
+void cannot_execute(int);
+void error_die(const char *);
+void execute_cgi(int, const char *, const char *, const char *);
+int get_line(int, char *, int);
+void headers(int, const char *);
+void not_found(int);
+void serve_file(int, const char *);
+int startup(u_short *);
+void unimplemented(int);
+
+
+
+#endif
