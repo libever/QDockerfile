7bdf96691be7777538150e32145a9853cf13670c
xingyue <qixingyue@126.com>
2015-12-21 19:06:46 +0800

    启动docker运行环境

--- mydocker.sh
+++ mydocker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 yum install docker
+systemctl enable docker.service
+systemctl start docker.service
 git clone https://github.com/lovely-codes/QDockerfile.git
 /bin/sh QDockerfile/create_xcentos7.sh
 cd QDockerfile/base
