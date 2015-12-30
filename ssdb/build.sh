717cf4d206bf7d6a58fd0cc516b8a867192757fe
xingyue <qixingyue@126.com>
2015-12-30 16:02:01 +0800

    添加需要的url包

--- ssdb/build.sh
+++ ssdb/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#need https://github.com/ideawu/ssdb/archive/master.zip
+
 #https://github.com/ideawu/ssdb
 #wget --no-check-certificate https://github.com/ideawu/ssdb/archive/master.zip
 docker build -t qdocker/ssdb:7 .
