124305e66aedb0454d1a3a052d6593f303ac94c1
xingyue <qixingyue@126.com>
2016-02-04 09:36:59 +0800

    -d 模式启动nginx

--- nginx/install_nginx.sh
+++ nginx/install_nginx.sh
@@ -3,3 +3,4 @@
 cd /sw/nginx-1.9.3
 ./configure --prefix=/nginx && make && make install
 
+echo "daemon off;" >> /nginx/conf/nginx.conf
--- nginx/start.sh
+++ nginx/start.sh
@@ -8,6 +8,6 @@ else
     d=$(readlink -f ../code)
 fi
 
-dparams="--name nginx -i -t -p 8000:80 -v $d:/code"
+dparams="--name nginx -d -p 10080:80 -v $d:/code"
 dparams="$dparams --privileged=true "
 screen -S dockernginx docker run $dparams qdocker/nginx:7 
