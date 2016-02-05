b0886934e85de60098d58f52bfe98bb1fb5e6927
xingyue <qixingyue@126.com>
2016-02-05 09:30:16 +0800

    mysql5.6安装需要判断状态

--- docker-compose-projects/lnmp/r.sh
+++ docker-compose-projects/lnmp/r.sh
@@ -5,7 +5,7 @@ cd download
 
 wget  http://download.openvz.org/template/precreated/centos-7-x86_64.tar.gz
 cat centos-7-x86_64.tar.gz | docker import - xcentos:7
-docker run -i -t xcentos:7 /bin/sh
+#docker run -i -t xcentos:7 /bin/sh
 
 git clone https://github.com/lovely-codes/QDockerfile.git
 
@@ -26,11 +26,5 @@ pwd
 sh build.sh
 cd ..
 
-cd mysql5.6
-pwd
-sh build.sh
-cd ..
-
-cd docker-compose-projects/lnmp
-docker-compose up -d 
+cd mysql5.6 && pwd && sh build.sh && cd ..  && cd docker-compose-projects/lnmp && docker-compose up -d 
 
