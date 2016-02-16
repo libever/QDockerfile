b0151474a8645519234ad19dcad5f2b0e44e9816
xingyue <qixingyue@126.com>
2016-02-16 16:18:55 +0800

    必要的时候使用rnleaf里边的docker

--- react-native-packager/build.sh
+++ react-native-packager/build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+echo ""
+echo "You can Alo USE rnleafdocker instead of this project: "
+echo "GIT DIR IS : "
+echo "https://github.com/qixingyue/rnleafdocker"
+echo "OR STILL USE THIS ANY KEY TO CONTINUE ..."
+read m
+
 docker build -t packager .
