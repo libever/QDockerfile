8d1991e40f9bb973deebd3db3e4ece577f2e5094
xingyue <qixingyue@126.com>
2016-02-17 11:45:05 +0800

    升级到golang1.5

---
+++ golang1.5/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "This version is golang1.5 "
+echo "ENTER TO CONTINUE ..."
+read m
+if [ -f ./go1.5.3.linux-amd64.tar.gz ]
+then
+	docker build -t qdocker/golang15:7 .
+else 
+	echo "can't find file : go1.5.3.linux-amd64.tar.gz"
+	echo "download...."
+	wget -t 3 --timeout 180 "http://www.golangtc.com/static/go/go1.5.3/go1.5.3.linux-amd64.tar.gz"
+	if [ $? -eq 0 ]
+	then
+		sh build.sh
+	else
+		rm go1.5.3.linux-amd64.tar.gz
+		echo "################"
+		echo "go1.5.3.linux-amd64.tar.gz get error ... try to change other version of golang ."
+	fi
+
+fi
---
+++ golang1.5/install_golang.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+mkdir -p /gocode
+
+cd /sw/golang15/go/src
+./all.bash
